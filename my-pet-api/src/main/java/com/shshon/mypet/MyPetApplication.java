92383f054bed58b820869970f16d0772a6d48531
sanghun92 <mue0329@gmail.com>
2023-02-05 22:29:46 +0900

    feat: 프로젝트 구성

---
+++ my-pet-api/src/main/java/com/shshon/mypet/MyPetApplication.java
@@ -0,0 +1,12 @@
+package com.shshon.mypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetApplication.class, args);
+    }
+}
